78c9f02a8935193264c9c2beb62debbb19078bb9
Денис Ракитин <piggest@mail.ru>
2018-05-26 11:34:06 +0300

    prepare interface

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Denis Rakitin on 26/05/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonWasPressed(_ sender: Any) {
+    }
+    
+
+}
+
---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,26 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Denis Rakitin on 26/05/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int){
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+
+}
