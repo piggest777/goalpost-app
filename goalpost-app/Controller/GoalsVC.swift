259040a381db55362a3595c84930318b2927e4e0
Денис Ракитин <piggest@mail.ru>
2018-05-27 20:18:00 +0300

    init coredata

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
@@ -20,6 +21,9 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let goal = Goal()
+        goal.goalCompletionValue = Int32(exactly: 12.0)
 
     }
 
