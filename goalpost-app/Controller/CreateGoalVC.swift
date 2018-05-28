6fb8a5ba5d7562a66452d5d69f947f473b8b803a
Денис Ракитин <piggest@mail.ru>
2018-05-28 14:09:28 +0300

    added some ext for button and UIview

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -19,10 +19,15 @@ class CreateGoalVC: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     
-    
+    //Variables
+    var goalType: GoalType = .shortTerm
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextButton.bindToKeyBoard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
 
     }
 
@@ -31,9 +36,15 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func longTermButtonWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        shortTermButton.setDeselectedColor()
+        longTermButton.setSelectedColor()
     }
     
     @IBAction func shortTermWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func nextButtonWasPressed(_ sender: Any) {
--- goalpost-app/Extensions/UIViewExt.swift
+++ goalpost-app/Extensions/UIViewExt.swift
@@ -10,6 +10,19 @@ import UIKit
 
 extension UIView {
     func bindToKeyBoard () {
-        NotificationCenter.default
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+   @objc func keyBoardWillChange(_ notification: NSNotification) {
+    let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+    let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+    let startingFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+    let endingFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+    let deltaY = endingFrame.origin.y - startingFrame.origin.y
+    
+    UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+        self.frame.origin.y += deltaY
+    }, completion: nil)
+    
     }
 }
